a3cf29eb8ea56dea19349feda38618147b6a89c6
Tracy Chen <stchen1012@gmail.com>
2020-09-29 15:50:48 -0400

    hello world endpoint added

--- demo/src/main/java/com/example/demo/controller/Controller.java
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Controller {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
 }
