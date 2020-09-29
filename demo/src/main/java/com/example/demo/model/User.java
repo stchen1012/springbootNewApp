82f98d00bbe9562cebb8a48960f669b11e388e78
Tracy Chen <stchen1012@gmail.com>
2020-09-29 16:17:26 -0400

    user model and app-dev props added. db created

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,36 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "users")
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    
+}
